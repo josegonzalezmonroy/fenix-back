3c523abc0c4a19cd4220d687b1b4b657f895d1de
josegonzalezmonroy <105132443+josegonzalezmonroy@users.noreply.github.com>
2025-03-05 22:51:56 -0300

    adiciona configuração de variáveis de ambiente e propriedades do banco de dados

--- src/main/java/com/projetofinal/backend/config/EnvConfiguration.java
+++ src/main/java/com/projetofinal/backend/config/EnvConfiguration.java
@@ -1,5 +1,15 @@
 package com.projetofinal.backend.config;
 
-public class EnvConfiguration {
+import org.springframework.context.annotation.Configuration;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class EnvConfiguration {
+    static {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+    }
 }
